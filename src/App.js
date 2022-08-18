d78e1f76f8ecba3ebd6200f4d283d59610eb855f
kkalid07 <khaled.khalfaoui@esprit.tn>
2022-08-18 12:25:21 +0200

    update our menuItems to navigate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
+import  {  Routes, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import ShopPage from './pages/shop/shop.component';
 
+
+  
 
 function App() {
-  return <div> 
-    <HomePage />
-        </div>;
+  return (
+    <div>
+      
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
+      </Routes>
+      
+      
+        
+    </div>
+  );
+  
+
+  
   
 }
 
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return( 
+        <div>
+            <h1>SHOP PAGE</h1>
+        </div>
+        );
+       
+    }
+}
+
+export default ShopPage;

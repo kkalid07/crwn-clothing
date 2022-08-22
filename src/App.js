ac38d31354c9194dbadc5bd0f46fc344dd4f819b
kkalid07 <khaled.khalfaoui@esprit.tn>
2022-08-22 14:26:52 +0200

    adding firebase to our project and auth

--- src/App.js
+++ src/App.js
@@ -6,24 +6,55 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
+import Header from '../src/components/header/header.component';
 
-  
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-function App() {
-  return (
-    <div>
-      
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/shop' element={<ShopPage/>} />
-      </Routes>
-         
-    </div>
-  );
-  
+import { auth } from './firebase/firebase.utils';
 
-  
-  
+
+class App extends React.Component {
+
+  constructor () {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState ({ currentUser:user});
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header currentUser = {this.state.currentUser}/> 
+        <Routes>
+          <Route exact path='/' element={<HomePage/>} />
+          <Route path='/shop' element={<ShopPage/>} />
+          <Route path='/sign-in' element={<SignInAndSignUpPage/>} />
+        </Routes>
+           
+      </div>
+    )
+  }
+
 }
+    
+  
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+
+const config = {
+    apiKey: "AIzaSyBzPz6u8aQ-Dij1Fz9haWnaxvbqM5-w9Ho",
+    authDomain: "crwn-db-40bf4.firebaseapp.com",
+    projectId: "crwn-db-40bf4",
+    storageBucket: "crwn-db-40bf4.appspot.com",
+    messagingSenderId: "434891305920",
+    appId: "1:434891305920:web:1ff50c143bfb6123068e25",
+    measurementId: "G-1M8RXCP0VJ"
+  };
+
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export  const signInWithGoogle = () =>auth.signInWithPopup(provider);
+
+  export default firebase;

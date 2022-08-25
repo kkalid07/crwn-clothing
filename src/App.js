62b44c24977165ed34cfeba703092094a6d5ecef
lenovo <khaled.khalfaoui@esprit.tn>
2022-08-25 17:21:34 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import  {  Routes, Route} from 'react-router-dom';
+import  {  Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -61,9 +61,13 @@ class App extends React.Component {
   }
 
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
     
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })  
 
-export default connect(null,mapDispatchToProps) (App);
+export default connect(mapStateToProps,mapDispatchToProps) (App);

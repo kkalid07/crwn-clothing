e199111f28f267c5f9bcd2e14adf25cd680eb89c
lenovo <khaled.khalfaoui@esprit.tn>
2022-08-30 11:26:46 +0200

    adding to cart action

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import  {  Routes, Route } from 'react-router-dom';
+import  {  Routes, Route  } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,11 @@ import './header.styles.scss';
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils"; 
-import { connect } from "react-redux";  
+import { connect } from "react-redux"; 
+import CartIcon from "../cart-icon/cart-icon.component"; 
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
       <Link className='logo-container' to='/'>
         <Logo className='logo' />
@@ -25,13 +27,19 @@ const Header = ({ currentUser }) => (
           <div><Link className='option' to='/sign-in'> SIGN IN</Link></div>
           
           
-        } 
+        }
+        <CartIcon /> 
       </div>
+      {
+        hidden ? null: <CartDropdown />
+      }
+      
     </div>
   );
 
-const mapStateToProps =state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps =({user: { currentUser }, cart : { hidden } }) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps) (Header);

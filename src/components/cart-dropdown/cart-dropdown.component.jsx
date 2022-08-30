e0401c08bffa45bd7fb812847a257037a15620a1
lenovo <khaled.khalfaoui@esprit.tn>
2022-08-30 12:30:55 +0200

    adding counting cart items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 import './cart-dropdown.styles.scss';
 
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from "../cart-item/cart-item.component";
 
 
-const CartDropdown = ()=> (
+const CartDropdown = ({ cartItems})=> (
 <div className="cart-dropdown">
-    <div className="cart-items"/>
+    <div className="cart-items">
+        {
+            cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/> )
+        }
+    </div>
     <CustomButton>CHECKOUT</CustomButton>
 </div>    
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
